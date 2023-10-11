92dd75a98b182f934ab87993f629c0a2d6b69073
Adetola Olatunde <oyinda.olatunde@gmail.com>
2023-10-11 23:13:06 +0100

    added views to hr data analysis

---
+++ hr_data_analysis.sql
@@ -0,0 +1,112 @@
+-- Questions
+-- ============
+-- What is the gender breakdown of employees in the company?
+-- What is the race/ethnicity breakdown of employees in the company?
+-- What is the age distribution of employees in the company?
+-- How many employees work at headquarters versus remote locations?
+-- What is the average length of employment for employees who have been terminated?
+-- How does the gender distribution/count vary across departments and job titles?
+-- What is the distribution/count of job titles across the company?
+-- Which department has the highest turnover rate?
+-- What is the distribution/count of employees across locations by city and state?
+-- How has the company's employee count changed over time based on hire and term dates?
+
+
+-- What is the gender breakdown of employees in the company?
+-- ==========================================================
+SELECT gender, COUNT(gender) num_of_employees
+FROM hr
+WHERE age >= 18 AND termdate IS NULL
+GROUP BY gender
+;
+
+
+-- What is the race/ethnicity breakdown of employees in the company?
+-- =================================================================
+SELECT race, COUNT(race) num_of_employees  
+FROM hr
+WHERE age >= 18 AND termdate IS NULL
+GROUP BY race
+;
+ 
+-- What is the age distribution of employees in the company?
+-- ==========================================================
+SELECT 
+  CASE 
+    WHEN age BETWEEN 18 AND 24 THEN '18-24'
+    WHEN age BETWEEN 25 AND 34 THEN '25-34'
+    WHEN age BETWEEN 35 AND 44 THEN '35-44'
+    WHEN age BETWEEN 45 AND 54 THEN '45-54'
+    WHEN age BETWEEN 55 AND 64 THEN '55-64'
+    ELSE '65+'
+  END AS age_group,
+  COUNT(*) AS num_of_employees
+FROM hr
+WHERE age >= 18 AND termdate IS NULL
+GROUP BY age_group
+ORDER BY age_group
+;
+
+-- How many employees work at headquarters versus remote locations?
+-- ================================================================
+SELECT location, COUNT(*) num_of_employees
+FROM hr
+WHERE age >= 18 AND termdate IS NULL
+GROUP BY location
+;
+
+-- What is the average length of employment for employees who have been terminated?
+-- =================================================================================
+SELECT ROUND(AVG(length_of_employment)) avg_length_of_employment
+FROM (SELECT YEAR(FROM_DAYS(DATEDIFF(termdate, hire_date))) length_of_employment 
+		FROM hr
+		WHERE age >= 18 AND 
+			  termdate IS NOT NULL AND
+              termdate < CURDATE()) temp
+;
+
+-- How does the gender distribution/count vary across departments and job titles?
+-- ===============================================================================
+SELECT department, jobtitle, gender, COUNT(*) num_of_employees
+FROM hr
+WHERE age >= 18 AND termdate IS NULL
+GROUP BY gender, department, jobtitle
+ORDER BY department
+;
+
+-- What is the distribution/count of job titles across the company?
+-- ======================================================================
+SELECT jobtitle, COUNT(*) num_of_employees 
+FROM hr
+WHERE age >= 18 AND termdate IS NULL
+GROUP BY jobtitle
+ORDER BY jobtitle
+;
+
+-- Which department has the highest turnover rate?
+-- =========================================================
+SELECT department,
+  total_count,
+  terminated_count,
+  terminated_count/total_count AS termination_rate
+FROM (
+  SELECT department,
+    COUNT(*) AS total_count,
+    SUM(CASE WHEN termdate IS NOT NULL AND termdate <= CURDATE() THEN 1 ELSE 0 END) AS terminated_count
+  FROM hr
+  WHERE age >= 18
+  GROUP BY department) AS t
+ORDER BY termination_rate DESC
+;
+
+-- What is the distribution/count of employees across locations by city and state?
+-- ===============================================================================
+SELECT location, location_state, location_city, COUNT(*) num_of_employees
+FROM hr
+WHERE age >= 18 AND termdate IS NULL
+GROUP BY location, location_city, location_state
+ORDER BY num_of_employees
+;
+
+-- How has the company's employee count changed over time based on hire and term dates?
+-- ====================================================================================
--- hr_data_cleaning.sql
+++ hr_data_cleaning.sql
@@ -6,7 +6,6 @@ USE human_resource;
 DESCRIBE hr;
 SELECT * FROM hr;
 
-
 -- Correcting the id field name
 -- ===========================================  
 ALTER TABLE hr
@@ -19,11 +18,16 @@ RENAME COLUMN ï»¿id TO id;
  
 UPDATE hr
 SET birthdate = CASE WHEN birthdate LIKE '%/%' THEN STR_TO_DATE(birthdate, "%m/%d/%Y")
-					 WHEN birthdate LIKE '%-%' THEN STR_TO_DATE(birthdate, "%m-%d-%Y")
+					 WHEN birthdate LIKE '%-%' THEN STR_TO_DATE(birthdate, "%m-%d-%y")
 				END;
                 
 ALTER TABLE hr
 MODIFY COLUMN birthdate DATE;
+
+-- Correct birthdates
+UPDATE hr
+SET birthdate = DATE_SUB(birthdate, INTERVAL 100 YEAR)
+WHERE birthdate >= CURDATE(); 
 
 UPDATE hr
 SET hire_date = CASE WHEN hire_date LIKE '%/%' THEN STR_TO_DATE(hire_date, '%m/%d/%Y')
@@ -37,10 +41,13 @@ UPDATE hr
 SET termdate = CASE WHEN termdate <> '' THEN LEFT(termdate, 19)
 					WHEN termdate = '0000-00-00 00:00:00' THEN NULL
 			   END;
-               
-UPDATE hr
-SET termdate = NULL
-WHERE termdate = '0000-00-00 00:00:00';
 
 ALTER TABLE hr
 MODIFY COLUMN termdate DATETIME;
+
+
+ALTER TABLE hr
+ADD COLUMN age INTEGER;
+ 
+UPDATE hr
+SET age = YEAR(FROM_DAYS(DATEDIFF(CURDATE(), birthdate)));
